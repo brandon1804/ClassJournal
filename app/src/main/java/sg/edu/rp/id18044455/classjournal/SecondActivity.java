63d8d03a3e43b9bb2233eed515cf145b3a2c693a
Michell <19030685@myrp.edu.sg>
2021-04-29 14:39:23 +0800

    Updated with activity_second.xml

---
+++ app/src/main/java/sg/edu/rp/id18044455/classjournal/SecondActivity.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.id18044455.classjournal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
