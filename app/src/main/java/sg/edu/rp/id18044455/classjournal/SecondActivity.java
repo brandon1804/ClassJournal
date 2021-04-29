ae948c5377c571bed68e4ead36ba0d6388243946
19031965 <19031965@myrp.edu.sg>
2021-04-29 15:59:33 +0800

    Third Commit, did INFO button - Mathias

--- app/src/main/java/sg/edu/rp/id18044455/classjournal/SecondActivity.java
+++ app/src/main/java/sg/edu/rp/id18044455/classjournal/SecondActivity.java
@@ -3,6 +3,7 @@ package sg.edu.rp.id18044455.classjournal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,6 +11,8 @@ import android.widget.Button;
 import java.util.ArrayList;
 
 public class SecondActivity extends AppCompatActivity {
+
+    Button btnInfo;
     Button btnEmail;
     ArrayList<DailyCA> dailyCA;
     @Override
@@ -41,6 +44,19 @@ public class SecondActivity extends AppCompatActivity {
                         "Choose an Email client :"));
 
             }});
+
+        btnInfo = findViewById(R.id.btnInfo);
+        btnInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                // Intent to display data
+                Intent infoIntent = new Intent(Intent.ACTION_VIEW);
+                // Set the URL to be used.
+                infoIntent.setData(Uri.parse("https://www.rp.edu.sg/soi/full-time-diplomas/details/diploma-in-digital-design-and-development"));
+                startActivity(infoIntent);
+            }
+        });
+
 
     }
 }
